7bbd7640a4551494f383e250a1288999eec86323
Thiago DevTAS <thiago@roadapps.net>
2014-09-08 22:18:50 -0300

    first version

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', {
+  	title: 'Chega de envias seus trabalhos como anexo para os clientes.. | Home',
+  	page: 'home'
+  });
+});
+
+/* GET sobre. */
+router.get('/sobre', function(req, res) {
+  res.render('about', {
+  	title: 'Sobre',
+  	page: 'about'
+  });
+});
+
+/* GET depoimentos. */
+router.get('/depoimentos', function(req, res) {
+  res.render('testimonials', {
+  	title: 'Depoimentos',
+  	page: 'testimonials'
+  });
+});
+
+module.exports = router;
