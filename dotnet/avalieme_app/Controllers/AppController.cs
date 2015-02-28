6178a38677f28686ec567c19c47cfd226676316c
Thiago DevTAS <thiago@roadapps.net>
2015-02-28 14:18:40 -0300

    Layer App

---
+++ dotnet/avalieme_app/Controllers/AppController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace avalieme_app.Controllers
+{
+    public class AppController : Controller
+    {
+        //
+        // GET: /App/
+
+        public ActionResult Index()
+        {
+            if (Request.IsAuthenticated)
+            {
+                ViewBag.Message = "App Fisrt Page";
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+    }
+}
